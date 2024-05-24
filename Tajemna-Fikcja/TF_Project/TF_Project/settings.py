c6fd8388e00bbebbe09f81f0b16e055b92cff27b
Laplaceov144 <112003546+Laplaceov144@users.noreply.github.com>
2024-05-24 20:07:13 +0200

    Update settings.py static xN

--- Tajemna-Fikcja/TF_Project/TF_Project/settings.py
+++ Tajemna-Fikcja/TF_Project/TF_Project/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = '/Tajemna-Fikcja/TF_Project/player/static'
 STATIC_URL = 'player/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
