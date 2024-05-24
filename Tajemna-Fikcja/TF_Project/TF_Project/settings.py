852826e3b095df5daf9b423a80f0de8f8dba11fe
Laplaceov144 <112003546+Laplaceov144@users.noreply.github.com>
2024-05-24 19:13:04 +0200

    Update settings.py static_urls

--- Tajemna-Fikcja/TF_Project/TF_Project/settings.py
+++ Tajemna-Fikcja/TF_Project/TF_Project/settings.py
@@ -116,8 +116,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
