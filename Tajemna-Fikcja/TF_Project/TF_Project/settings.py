c4cffb408a610535d173da591eda5b0d35f225f2
Laplaceov144 <112003546+Laplaceov144@users.noreply.github.com>
2024-05-24 13:48:06 +0200

    Update settings.py allowed hosts

--- Tajemna-Fikcja/TF_Project/TF_Project/settings.py
+++ Tajemna-Fikcja/TF_Project/TF_Project/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-3_ldy%n=rt(_y3rs3-p0qha69e20y#dez1$_sj)hp^m+w!(tf3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://tajemnafikcjaplejlisty.netlify.app/', 'http://127.0.0.1:8000/']
-# ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['https://tajemnafikcjaplejlisty.netlify.app/', 'http://127.0.0.1:8000/']
+ALLOWED_HOSTS = []
 
 
 # Application definition
