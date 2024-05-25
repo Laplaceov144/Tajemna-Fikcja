38ea925a8865e313c2cbbe7cf6e092cab7f79da3
Ego Death <jerzysusel@laplaceov144.local>
2024-05-25 11:54:16 +0200

    debug=false

--- Tajemna-Fikcja/TF_Project/TF_Project/settings.py
+++ Tajemna-Fikcja/TF_Project/TF_Project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3_ldy%n=rt(_y3rs3-p0qha69e20y#dez1$_sj)hp^m+w!(tf3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['https://tajemnafikcjaplejlisty.netlify.app/', 'http://127.0.0.1:8000/']
 ALLOWED_HOSTS = ['.vercel.app']
