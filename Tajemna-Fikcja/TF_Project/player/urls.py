a11f2492c22f8362c6e4248ca8791b43f330af56
Laplaceov144 <112003546+Laplaceov144@users.noreply.github.com>
2024-05-27 18:57:39 +0200

    Delete Tajemna-Fikcja directory

--- Tajemna-Fikcja/TF_Project/player/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path('api/submit-playlist/', views.submit_playlist, name='submit_playlist'),
-]
