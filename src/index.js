185412fbf4c7897f12c1d0ca7ef06417738615f5
Laplaceov144 <112003546+Laplaceov144@users.noreply.github.com>
2024-07-13 18:31:55 +0200

    Update index.js console logs

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 const container = document.getElementById('app');
+console.log(container);               //////////////
 const root = createRoot(container); 
 root.render(<App tab="home"/>);
 
