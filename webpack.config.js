669a9edccefef46848d8abd7667ba8417fbb17c9
Laplaceov144 <112003546+Laplaceov144@users.noreply.github.com>
2024-07-13 12:25:49 +0200

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js', 
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js' 
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  devServer: {
+    static: {
+        directory: path.join(__dirname, '/'),
+      },
+    historyApiFallback: true
+  },
+  mode: 'development'
+}
+
