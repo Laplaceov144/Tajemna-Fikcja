573191646a583945fc456d9ff08e7eb4fb075afe
Laplaceov144 <112003546+Laplaceov144@users.noreply.github.com>
2024-07-16 15:41:38 +0200

    Update webpack.config.js set dir to dist

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js', 
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js' 
   },
   module: {
