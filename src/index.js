bcc0a414a0bb350f22c094bedf564d4e0f4a39f6
Ego Death <jerzysusel@laplaceov144.local>
2024-07-17 17:15:14 +0200

    praca nad miniaturkom

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 const container = document.getElementById('app');
-console.log(container);               //////////////
 const root = createRoot(container); 
 root.render(<App tab="home"/>);
 
